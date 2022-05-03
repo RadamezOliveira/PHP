a32cfcf50412d1b7968c4e42e29bfd0c5bf62565
RadamezOliveira <radamezoliveira@gmail.com>
2022-05-02 23:30:20 -0300

    Aula03

---
+++ Aula03/index.php
@@ -0,0 +1,28 @@
+<?php
+
+$nomeCompleto = "Radaméz Oliveira";
+$idade = 39;
+$salario = 3.500;
+$salario1 = 4.500;
+$nomeAtivo = true;
+
+echo "Nome completo: " . $nomeCompleto;
+echo "<br>";
+echo $idade;
+echo "<br>";
+echo $salario;
+echo "<br>";
+echo $salario1;
+echo "<br>";
+echo "<br>";
+echo gettype($nomeCompleto); //echo gettype($nomeDaVariavel); retorna o tipo da variável
+echo "<br>";
+echo gettype($idade);
+echo "<br>";
+echo gettype($salario);
+echo "<br>";
+echo gettype($nomeAtivo);
+
+/* comentário
+de varias
+linhas */
