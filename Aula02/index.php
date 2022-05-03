1ac94264d59cd6c182e99f70a0182e8563dc10e7
RadamezOliveira <radamezoliveira@gmail.com>
2022-05-02 21:18:15 -0300

    Primeiro Commit

---
+++ Aula02/index.php
@@ -0,0 +1,8 @@
+<?php
+echo "Olá mundo!</br>"; //tanto aspas quanto apostrofos funcionam
+$nome = "<strong>Radaméz</strong>";
+echo "Meu nome é Radaméz Oliveira.</br>";
+echo "Meu nome é {$nome}.</br>";
+echo "Meu nome é " . $nome . ".</br>";
+print "Meu nome é Radaméz Oliveira.";
+?>
